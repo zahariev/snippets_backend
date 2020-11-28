3ef6f266d737ff7f72a30bb70dff6182579ed513
boyan zahariev <zaharievboyan@gmail.com>
2020-11-28 20:13:32 +0200

    add: mongoose

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
+// connect
+mongoose.connect(
+  "mongodb+srv://user11:password11@cluster0.74a0p.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  { useNewUrlParser: true },
+  () => console.log("connected to db")
+);
+
+// add routes
 const authRoute = require("./routes/auth");
 
 // middlewares
