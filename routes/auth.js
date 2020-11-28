63a836273e97143d7d405f2b576f881c130a2f23
boyan zahariev <zaharievboyan@gmail.com>
2020-11-28 21:51:44 +0200

    add: user props validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,21 @@
 const router = require("express").Router();
 const User = require("../model/User");
 
+// validation
+const Joi = require("@hapi/joi");
+
+const schema = Joi.object({
+  name: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(6).required(),
+});
+
 router.post("/register", async (req, res) => {
+  // validate before
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // register user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
