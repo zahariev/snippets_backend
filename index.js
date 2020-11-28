4597dedd9fa7e3361d6f5e08abca97e05848b52c
boyan zahariev <zaharievboyan@gmail.com>
2020-11-28 21:00:54 +0200

    add: user registration

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
+// add routes
+const authRoute = require("./routes/auth");
+
 // gets env config values
 dotenv.config();
 
@@ -10,11 +13,10 @@ dotenv.config();
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
   console.log("connected to db")
 );
-
-// add routes
-const authRoute = require("./routes/auth");
-
 // middlewares
-app.use("/api", authRoute);
+app.use(express.json());
+
+// Route middlewares
+app.use("/api/user", authRoute);
 
 app.listen(3000, () => console.log("Server started"));
