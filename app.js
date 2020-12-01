29e9ca14ab537f6050a4ae26f1cb1e672d0e6627
boyan zahariev <zaharievboyan@gmail.com>
2020-12-01 12:57:12 +0200

    add: validation , compression

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require("express");
-const app = express();
+
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-
+const compression = require("compression");
 // add routes
 const authRoute = require("./routes/auth");
 const snippetRoute = require("./routes/snippets");
 
+const app = express();
 // gets env config values
 dotenv.config();
-
+app.use(compression());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 // connect to mongodb
 mongoose.connect(
   process.env.DB_CONNECT,
@@ -23,4 +28,4 @@ app.use(express.json());
 app.use("/api/user", authRoute);
 app.use("/api/snippets", snippetRoute);
 
-app.listen(3000, () => console.log("Server started"));
+app.listen(3030, () => console.log("Server started"));
